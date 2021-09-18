d179681a1b9ab7f74e8757ce1cf7f18ed5da8d62
YellowAdas <patryk.brilla@gmail.com>
2021-09-18 14:41:42 +0200

    1 file modified

--- src/app/pokemon-detail/PokemonDetailStore/detailsReducers.ts
+++ src/app/pokemon-detail/PokemonDetailStore/detailsReducers.ts
@@ -1,4 +1,4 @@
-import { createReducer } from "@ngrx/store/src/reducer_creator";
+import { createReducer, on } from "@ngrx/store/src/reducer_creator";
 import { PokemonDetailsAbility, PokemonType } from "../../pokemon-details.model";
 
 export interface State {
@@ -20,63 +20,17 @@ export const initialState: State = {
     name : 'default name',
     height: 0,
     id: 0,
-    abilities: { 
-      ability: {
-        name : 'default punch',
-        url: 'default url'},
-      slot: 0
-    },
+    abilities: [],
     sprites: {
       front_default: 'default front',
       back_default: 'default back',
     },
-    types: {
-      type: {
-        name: 'default type name',
-        url: 'default type url'},
-      slot: 0
+    types: []
       }
     };
-  }
-}
-
-export const initialState: State = {
-  list: { pokemonList: [], error: '' },
-  pagination: {
-    currentPage: 0,
-    limit: 5,
-    totalCount: 0,
-  },
-  favorites: [],
-  isLoading: false,
-};
 
 export const ListReducer = createReducer<State>(
   initialState,
-  on(getList, (state, action) => ({ ...state, isLoading: true })),
+  on(actionName, (state, action) => ({ ...state, action.actionName }));
 
-
-
-  name: string;
-  height: number;
-  id: number;
-  abilities: PokemonDetailsAbility[];
-  sprites: {
-    front_default: string;
-    back_default: string;
-  };
-  types: PokemonType[];
-}
-
-ability: {
-  name: string;
-  url: string;
-};
-is_hidden: boolean;
-slot: number;
-
-type: {
-  name:string;
-  url:string;
-};
-slot: number;
+  export const pokemonDetailFeatureKey = 'PokemonDetail';
